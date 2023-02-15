44347ea4a206899b9e9541d3282be688ea471998
Shurley <55343840+DontCallMeShurley@users.noreply.github.com>
2023-02-15 23:29:38 +0300

    init

---
+++ Assets/Scripts/CubeCreate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeCreate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void onClick()
+    {
+        int i = Convert.ToInt32(GameObject.Find("InputField").GetComponent<InputField>().text);
+        for (int x = 1; x <= i; x++)
+        {
+            for (int y = 1; y <= i; y++)
+            {
+                for (int z = 1; z <= i; z++)
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(x, y, z) * 1.1f;
+                    cube.name = "Cube_" + x +"_" + y+"_" + z;
+                }
+            }
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/Keys.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keys : MonoBehaviour
+{
+    int k = 0;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            keyRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            keyLeft();
+        }
+    }
+
+    void keyLeft()
+    {
+        int i = int.Parse(GameObject.Find("InputField").GetComponent<InputField>().text);
+        ToggleCubes(true, i);
+        k++;
+        ToggleCubes(false, i);
+    }
+
+    void keyRight()
+    {
+        int i = int.Parse(GameObject.Find("InputField").GetComponent<InputField>().text);
+        ToggleCubes(true, i);
+        k--;
+        ToggleCubes(false, i);
+    }
+
+    void ToggleCubes(bool value, int i)
+    {
+        if (k >= 1 && k <= i)
+        {
+            for (int y = 1; y <= i; y++)
+            {
+                for (int z = 1; z <= i; z++)
+                {
+                    GameObject.Find($"Cube_{k}_{y}_{z}").GetComponent<Renderer>().enabled = value;
+                }
+            }
+        }
+    }
